33b0d528b1aac0e4d24e15f187aa5535675da4a2
Zachary Watkins <watkinza@gmail.com>
2017-06-02 10:13:31 -0500

    Improve compatibility with AgriFlex3 sites

--- single-job_posting.php
+++ single-job_posting.php
@@ -7,14 +7,18 @@
  * @since agriflex 1.0
  */
 
-get_header(); ?>
-
+get_header();
+if( !function_exists('genesis') ) : ?>
 		<div id="wrap">
 			<div id="content" role="main">
+<?php else : ?>
+		<div class="content-sidebar-wrap">
+			<main class="content">
+				<a id="content" title="content"></a>
+<?php endif;
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-			
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>	
+if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header">
 						<h1 class="entry-title"><?php the_title(); ?></h1>
 
@@ -23,16 +27,16 @@ get_header(); ?>
 							<?php agriflex_posted_on(); ?>
 						</div><!-- .entry-meta -->
 						<?php endif; ?>
-					</header><!-- .entry-header -->	
+					</header><!-- .entry-header -->
 					<section class="entry-content">
-						<?php 
-						
+						<?php
+
 						// Use Custom Fields (these will work with Gravity Forms submitted jobs
 						$custom = get_post_custom($post->ID);
-						
+
 						// Still Support the legacy _my_meta fields
-						$my_meta = get_post_meta($post->ID,'_my_meta',TRUE); 
-						
+						$my_meta = get_post_meta($post->ID,'_my_meta',TRUE);
+
 						$job_number 	= (is_array($my_meta)&&$my_meta['job_number']<>''? $my_meta['job_number'] : $custom["job_number"][0]);
 						$agency 		= (is_array($my_meta)&&$my_meta['agency']<>'' ? $my_meta['agency'] 		: $custom["agency"][0]);
 						$location		= (is_array($my_meta)&&$my_meta['location']<>'' ? $my_meta['location'] 	: $custom["location"][0]);
@@ -50,29 +54,44 @@ get_header(); ?>
 						$job_categories   = get_the_terms( $post->ID, 'job_category');
 						?>
 						<div class="job-posting-details">
-							<dl class="job-posting-dl">	
+							<dl class="job-posting-dl">
 							<?php if ($agency) { ?>
 							<dt class="job-posting-dt">Agency</dt>
 								<dd class="job-posting-dd"><?php echo $agency; ?></dd>
 							<?php } ?>
-							<?php if ($location) { ?> 
-							<dt class="job-posting-dt">Location</dt>	
-								<dd class="job-posting-dd"><?php echo $location;?></dd> 
+							<?php if ($location) { ?>
+							<dt class="job-posting-dt">Location</dt>
+								<dd class="job-posting-dd"><?php echo $location;?></dd>
 							<?php } ?>
-							<?php 
-							if ( $job_categories && ! is_wp_error( $job_categories ) ) {							
+							<?php
+							if ( $job_categories && ! is_wp_error( $job_categories ) ) {
 								$job_category_names = array();
 								foreach ( $job_categories as $term ) {
 									$job_category_names[] = $term->name;
-								}						
+								}
 								$the_job_category = join( ", ", $job_category_names );
 								?>
-								<dt class="job-posting-dt">Job Category</dt>	 
-								<dd class="job-posting-dd"><?php echo $the_job_category;?></dd> 
+								<dt class="job-posting-dt">Job Category</dt>
+								<dd class="job-posting-dd"><?php echo $the_job_category;?></dd>
 							<?php } ?>
-							
-							<?php if ($website) { ?>	
-							<dt class="job-posting-dt">Website</dt>							
+
+							<?php if ($salary) { ?>
+							<dt class="job-posting-dt">Salary</dt>
+								<dd class="job-posting-dd"><?php echo $salary;?></dd>
+							<?php } ?>
+
+							<?php if ($start_date) { ?>
+							<dt class="job-posting-dt">Start Date</dt>
+								<dd class="job-posting-dd"><?php echo $start_date;?></dd>
+							<?php } ?>
+
+							<?php if ($apply_date) { ?>
+							<dt class="job-posting-dt">Last Date to Apply</dt>
+								<dd class="job-posting-dd"><?php echo $apply_date;?></dd>
+							<?php } ?>
+
+							<?php if ($website) { ?>
+							<dt class="job-posting-dt job-posting-website">Website</dt>
 								<dd class="job-posting-dd"><?php
 								if($website[0] == "<" && $website[1] == "a"){
 									echo $website;
@@ -81,51 +100,36 @@ get_header(); ?>
 								}
 								?></dd>
 							<?php } ?>
-								
-							<?php if ($salary) { ?>	
-							<dt class="job-posting-dt">Salary</dt>							
-								<dd class="job-posting-dd"><?php echo $salary;?></dd>
-							<?php } ?>
-							
-							<?php if ($start_date) { ?>	
-							<dt class="job-posting-dt">Start Date</dt>							
-								<dd class="job-posting-dd"><?php echo $start_date;?></dd>
-							<?php } ?>
-							
-							<?php if ($apply_date) { ?>	
-							<dt class="job-posting-dt">Last Date to Apply</dt>							
-								<dd class="job-posting-dd"><?php echo $apply_date;?></dd>
-							<?php } ?>							
-														
-							<?php if ($description) { ?>	
-							<dt class="job-posting-dt">Description</dt>	
+
+							<?php if ($description) { ?>
+							<dt class="job-posting-dt job-posting-description">Description</dt>
 								<dd class="job-posting-dd"><?php echo $description;?></dd>
-							<?php } ?>	
-								
-							<?php if ($qualifications) { ?>
-							<dt class="job-posting-dt">Qualifications</dt>	 
-								<dd class="job-posting-dd"><?php echo $qualifications;?></dd> 
 							<?php } ?>
-							
+
+							<?php if ($qualifications) { ?>
+							<dt class="job-posting-dt job-posting-qualifications">Qualifications</dt>
+								<dd class="job-posting-dd"><?php echo $qualifications;?></dd>
+							<?php } ?>
+
 							<?php if ($job_number) { ?>
-							<dt class="job-posting-dt">Job Number</dt>											
+							<dt class="job-posting-dt">Job Number</dt>
 								<dd class="job-posting-dd"><?php echo $job_number;?></dd>
 							<?php } ?>
-							
-							<?php if ($contact_name) { ?>	 	
+
+							<?php if ($contact_name) { ?>
 							<dt class="job-posting-dt">Contact Person</dt>
 								<dd class="fn job-posting-dd"><?php echo $contact_name;?></dd>
 							<?php } ?>
-								
-							<?php if ($contact_phone) { ?>	
-							<dt class="job-posting-dt">Contact Phone</dt>	
-								<dd class="tel job-posting-dd"><?php echo $contact_phone;?></dd> 
+
+							<?php if ($contact_phone) { ?>
+							<dt class="job-posting-dt">Contact Phone</dt>
+								<dd class="tel job-posting-dd"><?php echo $contact_phone;?></dd>
 							<?php } ?>
-								
-							<?php if ($contact_email) { ?>	
-							<dt class="job-posting-dt">Contact eMail</dt>							
-								<dd class="email job-posting-dd"><a href="mailto:<?php echo $contact_email;?>"><?php echo $contact_email;?></a></dd> 
-							<?php } ?>		
+
+							<?php if ($contact_email) { ?>
+							<dt class="job-posting-dt">Contact eMail</dt>
+								<dd class="email job-posting-dd"><a href="mailto:<?php echo $contact_email;?>"><?php echo $contact_email;?></a></dd>
+							<?php } ?>
 
 							<?php if ($fileupload) { ?>
 							<dt class="job-posting-dt">File</dt>
@@ -138,7 +142,7 @@ get_header(); ?>
 								?></dd>
 							<?php } ?>
 							</dl>
-								
+
 						</div>
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'agriflex' ), 'after' => '</div>' ) ); ?>
 					</section><!-- .entry-content -->
@@ -149,7 +153,7 @@ get_header(); ?>
 						</section><!-- .entry-utility -->
 					</footer><!-- .entry-meta -->
 				</article><!-- #post-<?php the_ID(); ?> -->
-				
+
 				<nav id="nav-below" class="navigation">
 					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'agriflex' ) . '</span> %title' ); ?></div>
 					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'agriflex' ) . '</span>' ); ?></div>
@@ -157,9 +161,14 @@ get_header(); ?>
 
 				<?php comments_template( '', true ); ?>
 
-<?php endwhile; // end of the loop. ?>
+<?php endwhile; // end of the loop.
+if( !function_exists('genesis') ) : ?>
 			</div><!-- #content -->
 		</div><!-- #wrap -->
-
-<?php get_sidebar(); ?>
+		<?php get_sidebar(); ?>
+<?php else : ?>
+			</main>
+			<?php get_sidebar(); ?>
+		</div>
+<?php endif; ?>
 <?php get_footer(); ?>
