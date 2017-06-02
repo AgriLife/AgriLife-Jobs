aa7726c4829553f9434a22ff82c9aa30df36dbdd
Zachary Watkins <watkinza@gmail.com>
2017-06-02 15:56:40 -0500

    Improve appearance for AgriFlex3 on search results page

--- archive-job_posting.php
+++ archive-job_posting.php
@@ -4,15 +4,27 @@
  * @package WordPress
  */
 
-get_header(); ?>
+get_header();
 
+if( !function_exists('genesis') ) : ?>
 		<div id="wrap">
 			<div id="content" role="main">
+<?php else : ?>
+		<div class="content-sidebar-wrap">
+			<main class="content">
+				<a id="content" title="content"></a>
+<?php endif; ?>
 				<h1 class="entry-title">Jobs</h1>
-				<?php include( 'loop-job_listings.php' ); ?>
-					
+				<?php include( 'loop-job_listings.php' );
+
+if( !function_exists('genesis') ) : ?>
 			</div><!-- #content -->
 		</div><!-- #wrap -->
+		<?php get_sidebar(); ?>
+<?php else : ?>
+			</main>
+			<?php get_sidebar(); ?>
+		</div>
+<?php endif;
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+get_footer(); ?>
